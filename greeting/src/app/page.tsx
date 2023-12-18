a1c9c365790306745dd4f6d2e0d0153d7fb4bfa2
LucieneFreitas <luciene_freitas@yahoo.com.br>
2023-12-18 11:46:26 -0300

    atualização

---
+++ greeting/src/app/page.tsx
@@ -0,0 +1,27 @@
+const Page = () => {
+  const fullTime = new Intl.DateTimeFormat('pt-br', {
+    timeStyle: 'short',
+    hour12: false,
+  }).format();
+
+  const hour = new Date().getHours();
+  let greeting = '';
+
+  if (hour >= 0 && hour < 12) {
+    greeting = 'Bom dia 😍'
+  } else if (hour >= 12 && hour < 18) {
+    greeting = 'Boa tarde 😎'
+  } else if (hour >= 18 && hour <= 23) {
+    greeting = 'Boa noite 😴'
+  }
+
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center items-center text-white bg-gradient-to-r from-sky-500 to-indigo-500">
+      <div className="text-9xl">{fullTime}</div>
+      <div className="text-5xl font-bold">{greeting}</div>
+    </div>
+  )
+}
+
+export default Page;
+
